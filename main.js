48aa177c21047525b921b6469b52ccee7a84677f
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2021-03-22 21:26:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function find() {
+    var x = document.getElementById("az").value;
+    document.getElementById("1a").innerHTML = x + "म्";
+    document.getElementById("2a").innerHTML = x + "म्";
+    document.getElementById("3a").innerHTML = x + "ेेन";
+    document.getElementById("4a").innerHTML = x + "ाय";
+    document.getElementById("5a").innerHTML = x + "ात्";
+    document.getElementById("6a").innerHTML = x + "स्य";
+    document.getElementById("7a").innerHTML = x + "े";
+    document.getElementById("sa").innerHTML = "हे " + x + "म् !";
+
+    document.getElementById("1b").innerHTML = x + "े";
+    document.getElementById("2b").innerHTML = x + "े";
+    document.getElementById("3b").innerHTML = x + "ाभ्याम्";
+    document.getElementById("4b").innerHTML = x + "ाभ्याम्";
+    document.getElementById("5b").innerHTML = x + "ाभ्याम्";
+    document.getElementById("6b").innerHTML = x + "योः";
+    document.getElementById("7b").innerHTML = x + "योः";
+    document.getElementById("sb").innerHTML = "हे " + x + "े !";
+
+    document.getElementById("1c").innerHTML = x + "ानि";
+    document.getElementById("2c").innerHTML = x + "ानि";
+    document.getElementById("3c").innerHTML = x + "ैः";
+    document.getElementById("4c").innerHTML = x + "ेभ्य";
+    document.getElementById("5c").innerHTML = x + "ेभ्य";
+    document.getElementById("6c").innerHTML = x + "ानाम्";
+    document.getElementById("7c").innerHTML = x + "ेषु";
+    document.getElementById("sc").innerHTML = "हे " + x + "ानि !";
+
+    document.getElementById("bb").innerHTML = x + " के शब्द रूप";
+    document.title =  x + " के शब्द रूप | Shivam's learning Website" ;
+}
